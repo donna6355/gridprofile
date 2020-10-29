1b549c6efcf85bf041884af426794514c4a19c4e
donna6355 <donnajun1214@gmail.com>
2020-10-29 16:07:19 +0900

    who and basic js done

--- js/profile.js
+++ js/profile.js
@@ -1,3 +1,4 @@
+// get the length of svg logo
 $(function(){
 // get the path length of svg logo
 const $signature = $('#fullSig>path');
@@ -5,5 +6,23 @@ console.log($signature.length)
 for(let i = 0; i<$signature.length;i++){
   console.log(`${i}/${$signature[i].id}'s path length = ${$signature[i].getTotalLength()}`);
 }
+})
+
+// real animatio for full page
+$(function(){
+  $article = $('.tit');
+  setTimeout(function(){
+    $('.intro').fadeOut();
+  }
+    ,6000);
+    setTimeout(function(){
+      $article.eq(0).addClass('turn');
+    },6200);
+    setTimeout(function(){
+      $article.eq(1).addClass('turn');
+    },8000);
+    setTimeout(function(){
+      $article.eq(2).addClass('turn');
+    },9800);
 
 })
